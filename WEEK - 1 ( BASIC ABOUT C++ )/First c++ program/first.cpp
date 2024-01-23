2b00fcdfcdaf64017b469722b7a0d4b422e1e4ea
ritik2601 <124141120+ritik2601@users.noreply.github.com>
2024-01-23 21:56:00 +0530

    Add files via upload

---
+++ WEEK - 1 ( BASIC ABOUT C++ )/First c++ program/first.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int a = 10;
+int b = 5;
+
+cout<< (a != b) << endl;
+// cout<< a + b <<endl;
+// cout<< a - b <<endl;
+// cout<< a / b <<endl;
+// cout<< a * b <<endl;
+// cout<< a % b <<endl;
+
+
+
+// garbage value if their is not any value of num is decalred;
+    // int num;
+    // int num = 28;
+    // cout<< num <<endl;
+    // cout<<"Ritik" <<endl <<" Raj" <<'\n';
+    // cout <<"hello ritik";  
+
+// int --> 4 byte 
+
+// int num = 50;
+// cout<< num  << endl ;
+
+// cout<< sizeof(num) <<endl;
+
+// char ch = 'k';
+
+// cout<< ch <<endl;
+
+// // char--> 1 byte 
+// cout<< sizeof(ch) <<endl;
+
+// float R = 1.65;
+
+// cout<< R <<endl;
+
+// // float--> 4byte
+// cout<< sizeof(R)<<endl;
+
+// long pointer = 234;
+// cout<< pointer<<endl;
+
+// // long--> 8 byte
+// cout<<sizeof(pointer)<<endl;
+
+
+
+}
+
