a5ed11f0355b81d3d157d3975915b91c3ab9fd08
ritik2601 <124141120+ritik2601@users.noreply.github.com>
2024-01-28 21:07:31 +0530

    Add files via upload

---
+++ WEEK - 1 ( BASIC ABOUT C++ )/First c++ program/3rd.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int a = 10;
+int b = 5;
+
+// cout<< (a != b) << endl;
+ cout<< a + b <<endl;
+// cout<< a - b <<endl;
+// cout<< a / b <<endl;
+// cout<< a * b <<endl;
+// cout<< a % b <<endl;
+
+
+
+// garbage value if their is not any value of num is decalred;
+    // int num;
+    // int num = 28;
+    // cout<< num <<endl;
+    // cout<<"Ritik" <<endl <<" Raj" <<'\n';
+    // cout <<"hello ritik";  
+
+// int --> 4 byte 
+
+// int num = 50;
+// cout<< num  << endl ;
+
+// cout<< sizeof(num) <<endl;
+
+// char ch = 'k';
+
+// cout<< ch <<endl;
