7c39427c996928f192fc3a9e34e659ed55e81462
ritik2601 <124141120+ritik2601@users.noreply.github.com>
2024-01-29 21:33:52 +0530

    Update square.cpp

--- WEEK - 1 ( BASIC ABOUT C++ )/Pattern part 1/square.cpp
+++ WEEK - 1 ( BASIC ABOUT C++ )/Pattern part 1/square.cpp
@@ -1,1 +1,16 @@
-#
+#include<iostream>
+using namespace std;
+
+int main(){
+
+for(int i= 0; i<5; i=i+1){
+
+    for(int j=0; j<5;j=j+1){
+        cout<<" * " ; 
+    }
+ cout << endl;
+}
+
+return 0;
+
+}
