ae2aba7afe6a901b8bc9e8cc261060d9e69ab63f
ritik2601 <124141120+ritik2601@users.noreply.github.com>
2024-01-30 20:39:31 +0530

    Add files via upload

---
+++ WEEK - 1 ( BASIC ABOUT C++ )/Pattern part 1/Rectangle.cpp
@@ -0,0 +1,28 @@
+  #include<iostream>
+  using namespace std;
+
+  int main(){
+
+  for(int i=0; i<3; i=i+1){
+
+      for(int j=0; j<5; j=j+1){
+          cout<<" * ";
+      }
+  cout<< endl;
+
+
+ 
+
+
+}
+
+
+ 
+
+ }
+
+
+  
+
+
+
