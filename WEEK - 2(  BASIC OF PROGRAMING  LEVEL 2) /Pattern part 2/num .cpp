21db06a021ab123c0c85570c53dd8be88269d036
ritik2601 <124141120+ritik2601@users.noreply.github.com>
2024-02-02 21:30:35 +0530

    Add files via upload

---
+++ WEEK - 2(  BASIC OF PROGRAMING  LEVEL 2) /Pattern part 2/num .cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    for(int row=0; row<n; row=row+1){
+
+// --->inner loop
+for(int col=0; col<2*row+1; col=col+1){
+    if(col%2 == 1){
+        // ---> odd number
+        cout <<"*"; 
+    }
+    else{
+        cout <<row+1;
+    }
+}
+cout << endl;
+
+    }
+}
