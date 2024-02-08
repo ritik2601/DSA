c99758a51ae3c544329ab0c9373525eaf6b20570
ritik2601 <124141120+ritik2601@users.noreply.github.com>
2024-02-08 21:14:17 +0530

    Add files via upload

---
+++ WEEK - 2(  BASIC OF PROGRAMING  LEVEL 2)/Bitwise operators & loops/Xor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // here with same value it will give (0) in xor
+
+    //  int a=5;
+    //  int b=5;
+    // and withj diff with value it will give (0)
+    
+
+     int a=5;
+     int b=10;
+
+    cout<< (a ^ b) <<endl;
+}
